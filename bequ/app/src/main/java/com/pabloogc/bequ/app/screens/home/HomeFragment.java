ae337084f583c5266bda8a6e707a2c92f90fe7a6
Pablo Orgaz <pabloogc@gmail.com>
2014-04-12 18:15:17 +0200

    Added the books list

--- bequ/app/src/main/java/com/pabloogc/bequ/app/screens/home/HomeFragment.java
+++ bequ/app/src/main/java/com/pabloogc/bequ/app/screens/home/HomeFragment.java
@@ -2,70 +2,123 @@ package com.pabloogc.bequ.app.screens.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ScrollView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import com.android.volley.Request;
 import com.dropbox.client2.DropboxAPI;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.pabloogc.bequ.app.R;
 import com.pabloogc.bequ.app.screens.meta.BaseFragment;
 import com.pabloogc.bequ.app.util.Time;
+import com.pabloogc.bequ.app.widget.WLoadingLayout;
 import com.pabloogc.playa.handlers.SuccessHandler;
-import com.pabloogc.playa.handlers.impl.DialogLoadingHandler;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by Pablo Orgaz - 4/12/14 - pabloogc@gmail.com - https://github.com/pabloogc
  */
 public class HomeFragment extends BaseFragment {
 
-    protected TextView text;
-    protected DialogLoadingHandler loadingHandler;
+    protected @InjectView(R.id.booksListView) ListView listView;
+    protected @InjectView(R.id.loadingLayout) WLoadingLayout loadingLayout;
+    protected List<DropboxAPI.Entry> data = new ArrayList<DropboxAPI.Entry>(0);
+    protected BookAdapter adapter;
 
-    @Override public void onDetach() {
-        if (loadingHandler != null && loadingHandler.getDialog() != null)
-            loadingHandler.getDialog().cancel();
-        super.onDetach();
+    @Override public void onCreate(Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        doSearch();
+    }
+
+    @Override public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.books_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ScrollView scrollView = new ScrollView(inflater.getContext());
-        text = new TextView(inflater.getContext());
-        text.setOnClickListener(new View.OnClickListener() {
+        View root = inflater.inflate(R.layout.home_fragment, container, false);
+        ButterKnife.inject(this, root);
+        adapter = new BookAdapter();
+        listView.setAdapter(adapter);
+        root.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
                 doSearch();
             }
         });
-        text.setText("Loading . . . \n\n\n\n\n\n\n\n . . .");
-        //TODO: Use the helper
-        scrollView.addView(text);
-        return scrollView;
+        return root;
     }
 
     public void doSearch() {
-        if (loadingHandler == null)
-            loadingHandler = new DialogLoadingHandler(getActivity());
-
-        Request<?> request = getApiHelper().search("/", ".pdf", 0, false)
+        getApiHelper().search("/", ".pdf", 100, false)
                 .success(new SuccessHandler<List<DropboxAPI.Entry>>() {
                     @Override public void onSuccess(List<DropboxAPI.Entry> result) {
-                        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                        text.setText(gson.toJson(result));
+                        data = filterBooks(result);
+                        adapter.notifyDataSetChanged();
                     }
 
                     @Override public String getSuccessMessage(List<DropboxAPI.Entry> response) {
                         return String.format("Found %d", response.size());
                     }
                 })
-                .loading(loadingHandler)
-                .cacheTime(Time.seconds(30), Time.seconds(60))
+                .loading(loadingLayout)
+                .cacheTime(Time.seconds(30), Time.seconds(120))
                 .execute();
 
-        loadingHandler.setRequest(request);
+    }
+
+    private List<DropboxAPI.Entry> filterBooks(List<DropboxAPI.Entry> items) {
+        List<DropboxAPI.Entry> filter = new ArrayList<DropboxAPI.Entry>();
+        for (DropboxAPI.Entry book : items) {
+            if (book.fileName().endsWith(".pdf") && !book.fileName().startsWith("._"))
+                filter.add(book);
+        }
+        return filter;
+    }
+
+    protected static class Holder {
+        @InjectView(R.id.bookTitle) TextView title;
+    }
+
+    private class BookAdapter extends BaseAdapter {
+
+        @Override public int getCount() {
+            return data.size();
+        }
+
+        @Override public Object getItem(int position) {
+            return data.get(position);
+        }
+
+        @Override public long getItemId(int position) {
+            return data.get(position).fileName().hashCode();
+        }
+
+        @Override public View getView(int position, View convertView, ViewGroup parent) {
+            Holder holder;
+            if (convertView == null) {
+                convertView = View.inflate(parent.getContext(), R.layout.book_list_item, null);
+                holder = new Holder();
+                convertView.setTag(holder);
+                ButterKnife.inject(holder, convertView);
+            } else {
+                holder = (Holder) convertView.getTag();
+            }
+            holder.title.setText(data.get(position).fileName());
+            return convertView;
+        }
     }
 }
